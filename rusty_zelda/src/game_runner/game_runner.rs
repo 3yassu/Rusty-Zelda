c45960cd97ad78ce9434864f6a0b5d78ae901098
3yassu <68251430+3yassu@users.noreply.github.com>
2025-04-05 01:10:03 -0500

    create game_runner.rs | begin working on GameRunner

---
+++ rusty_zelda/src/game_runner/game_runner.rs
@@ -0,0 +1,15 @@
+pub struct GameRunner{
+    fps: u32,
+    //graphics: SDL2 STUFF (WORRY AB LATER) 
+}
+impl GameRunner{
+    pub fn new(fps: u32) -> Self {
+        Self{fps}
+    }
+    pub fn update(&mut self, delta_time: u32){ //maybe make sub_functions for update to make code more readable
+
+    }
+    pub fn render(&mut self){ //maybe make sub_functions for render to make code more readable
+         
+    }
+}
---
+++ rusty_zelda/target/debug/deps/rusty_zelda.d
@@ -0,0 +1,5 @@
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\debug\deps\rusty_zelda.exe: src\main.rs
+
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\debug\deps\rusty_zelda.d: src\main.rs
+
+src\main.rs:
