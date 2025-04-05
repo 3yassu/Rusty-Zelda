a07ab2e018deb0b327583aa541f4b0de026c0852
3yassu <3yassu@gmail.com>
2025-04-05 00:11:00 -0500

    Copy from github

---
+++ rusty_zelda/target/debug/deps/rusty_zelda-67de997a690a8900.d
@@ -0,0 +1,5 @@
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\debug\deps\librusty_zelda-67de997a690a8900.rmeta: src\main.rs
+
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\debug\deps\rusty_zelda-67de997a690a8900.d: src\main.rs
+
+src\main.rs:
---
+++ rusty_zelda/target/debug/deps/rusty_zelda-e7eb4f41e0969450.d
@@ -0,0 +1,5 @@
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\debug\deps\librusty_zelda-e7eb4f41e0969450.rmeta: src\main.rs
+
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\debug\deps\rusty_zelda-e7eb4f41e0969450.d: src\main.rs
+
+src\main.rs:
