77466da7e4f9dd94b83857a59bb8e3a35b3ac314
3yassu <3yassu@gmail.com>
2025-04-05 14:44:04 -0500

    Begin implementing collision for felix :)

--- rusty_zelda/src/game_runner/objects/felix.rs
+++ rusty_zelda/src/game_runner/objects/felix.rs
@@ -1,4 +1,14 @@
 use super::item;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+const TILE_SIZE: u32 = 32;
+const ROOM_WIDTH: u32 = 512;
+const ROOM_HEIGHT: u32 = 352;
+const MAP_WIDTH: usize = 16;
+const MAP_HEIGHT: usize = 11;
 pub struct Felix{ //Name of character (Fe)lix RedOx haha!
     inventory: Vec<item::Item>,
     health_bar: u8, //2*(heart count)
@@ -32,5 +42,27 @@ impl Felix{
                 }
             }
         }
+    }
+    fn rect(&self) -> Rect{
+        Rect::new(self.x as i32, self.y as i32, self.size, self.size)
+    }
+    fn in_collision(x: f32, y:f32, size: f32, word_dungeon: Vec<Vec<u8>>, enem_corner: [(f32, f32); 4]) -> bool {
+        let corners: [(f32, f32); 4]  = [
+            (x, y),
+            (x + size - 1.0, y),
+            (x, y + size - 1.0),
+            (x + size - 1.0, y + size - 1.0),
+        ];
+        for (cx, cy) in corners {
+            let tile_x = (cx/TILE_SIZE as f32) as usize;
+            let tile_y = (cy/TILE_SIZE as f32) as usize;
+            if word_dungeon[tile_y][tile_x] % 2 == 1 {return true};
+            if (cx <= enem_corner[1].0 && cx <= enem_corner[3].0) 
+                && 
+                (cy <= enem_corner[1].1 && cy <= enem_corner[2].1){
+                return true;
+            }
+        }
+        false
     }
 }
