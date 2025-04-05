12e31b56343aa48d55bd8c96aee5d02b0fcf8f79
3yassu <3yassu@gmail.com>
2025-04-05 15:27:44 -0500

    Make it run (on windows for now?)

---
+++ rusty_zelda/target/debug/deps/rusty_zelda-37c0fae6e0dcb2c8.d
@@ -0,0 +1,12 @@
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\debug\deps\librusty_zelda-37c0fae6e0dcb2c8.rmeta: src\main.rs src\game_runner.rs src\game_runner\objects\mod.rs src\game_runner\objects\item.rs src\game_runner\objects\felix.rs src\game_runner\objects\npc.rs src\game_runner\objects\world_runner.rs src\game_runner\objects\room_data.rs
+
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\debug\deps\rusty_zelda-37c0fae6e0dcb2c8.d: src\main.rs src\game_runner.rs src\game_runner\objects\mod.rs src\game_runner\objects\item.rs src\game_runner\objects\felix.rs src\game_runner\objects\npc.rs src\game_runner\objects\world_runner.rs src\game_runner\objects\room_data.rs
+
+src\main.rs:
+src\game_runner.rs:
+src\game_runner\objects\mod.rs:
+src\game_runner\objects\item.rs:
+src\game_runner\objects\felix.rs:
+src\game_runner\objects\npc.rs:
+src\game_runner\objects\world_runner.rs:
+src\game_runner\objects\room_data.rs:
