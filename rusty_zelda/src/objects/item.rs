0253791d6c28e7c68298beab37f2c6d22ab82fa4
Lillian Brooks-Kanost <142836967+lbrookskanost@users.noreply.github.com>
2025-04-04 22:47:08 -0500

    use function

--- rusty_zelda/src/objects/item.rs
+++ rusty_zelda/src/objects/item.rs
@@ -13,6 +13,10 @@ impl Item{
     pub fn new(id: u32, cost: i32, collision: bool, location: (Option<i32>, Option<i32>)) -> Self { 
         Self{id, cost, collision, location}
     }
+
+    pub fn use((i32, i32)){
+        match id //based on item id, functionality differs.
+    }
 }
 
 fn main(){
