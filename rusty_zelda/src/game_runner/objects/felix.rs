84679c37780db7a69c9b9b120293985f28b2ace2
3yassu <3yassu@gmail.com>
2025-04-05 16:44:11 -0500

    trying to get compile working for linux

--- rusty_zelda/src/game_runner/objects/felix.rs
+++ rusty_zelda/src/game_runner/objects/felix.rs
@@ -22,8 +22,11 @@ impl Felix{
     pub fn new(size: u32) -> Self {
         Self{inventory: vec!(), health_bar: 6, rupee_balance: 0, hand: (None, None), location: (0, 0), size, speed: 0.0}
     }
-    pub fn move_felix(&mut self, x: u32, y: u32){ //Keep in mind this function doesn't teleport felix but add's given position
-        self.location.0 += x; self.location.1 += y;
+    pub fn move_felix(&mut self, x: u32, y: u32, pos: bool){ //Keep in mind this function doesn't teleport felix but add's given position
+        match pos{
+            true => {self.location.0 += x; self.location.1 += y;},
+            false => {self.location.0 -= x; self.location.1 -= y;},
+        }
     }
     pub fn use_hand_a(&mut self){
         if let Some(item) = self.hand.0.as_mut(){
