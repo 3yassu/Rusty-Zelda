ef741dcd61ce8d2161bf9a3f11ca3ed4d21b8e0f
3yassu <68251430+3yassu@users.noreply.github.com>
2025-04-04 23:46:31 -0500

    Add files via upload

--- rusty_zelda/src/objects/felix.rs
+++ rusty_zelda/src/objects/felix.rs
@@ -13,5 +13,24 @@ impl Felix{
     pub fn move_felix(&mut self, x: i32, y: i32){ //Keep in mind this function doesn't teleport felix but add's given position
         self.location.0 += x; self.location.1 += y;
     }
-    //pub fn  
+    pub fn use_hand_a(&mut self){
+        if let Some(item) = self.hand.1.as_mut(){
+            item.use(self.location, self.rupee_balance);
+            if item.is_disposable(){
+                if item.count_remove() == 0 {
+                    item = None;
+                }
+            }
+        }
+    }
+    pub fn use_hand_b(&mut self){
+        if let Some(item) = self.hand.1.as_mut(){
+            item.use(self.location, self.rupee_balance);
+            if item.is_disposable(){
+                if item.count_remove() == 0 {
+                    item = None;
+                }
+            }
+        }
+    }
 }
