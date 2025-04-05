d2955f3efbb9ac895fee87bf08e923943f8b05c0
Lillian Brooks-Kanost <142836967+lbrookskanost@users.noreply.github.com>
2025-04-05 10:55:33 -0500

    rsss

--- rusty_zelda/src/game_runner/objects/room_data.rs
+++ rusty_zelda/src/game_runner/objects/room_data.rs
@@ -1,13 +1,15 @@
 //Nothing for now :)
 #[derive(Debug)]
 pub struct ShopRoomData{//room item info & image data
-    //dimensions: (i32, i32),
+    dimensions: (usize, usize),
+    dungeon: [[u8; dimensions.0]; dimensions.1],
     shopkeeper: Shopkeeper::new() //shopkeeper handles items, etc
 }
+
 #[derive(Debug)]
 pub struct HostileRoomData{
-    //dimensions: (i32, i32),
+    dimensions: (usize, usize),
+    dungeon: [[u8; dimensions.0]; dimensions.1],
     enemies: Vec<Enemy::new()>,
-    items: Vec<Item::new()>,
-    teleports: Vec<(i32, i32)> //special teleports in the middle of room
+    items: Vec<Item::new()>
 }
