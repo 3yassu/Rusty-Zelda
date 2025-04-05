b69e3068c8decc0b2d5f8198eddd941314ded073
3yassu <68251430+3yassu@users.noreply.github.com>
2025-04-04 22:34:32 -0500

    Update felix.rs

--- rusty_zelda/src/objects/felix.rs
+++ rusty_zelda/src/objects/felix.rs
@@ -1,8 +1,17 @@
 mod item;
-struct Felix{
+pub struct Felix{ //Name of character (Fe)lix RedOx haha!
     inventory: Vec<Item>,
-    health_bar: u32,
+    health_bar: u8, //2*(heart count)
     rupee_balance: i32,
     hand: (Option<Item>, Option<Item>),
     location: (i32, i32),
+}
+impl Felix{
+    pub fn new() -> Self {
+        Self{inventory: 0, health_bar: 6, rupee_balance: 0, hand: (None, None), location: (0, 0)}
+    }
+    pub fn move_felix(&mut self, x: i32, y: i32){ //Keep in mind this function doesn't teleport felix but add's given position
+        self.location.0 += x; self.location.1 += y;
+    }
+    //pub fn  
 }
