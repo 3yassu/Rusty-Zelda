6677bd5a4c97347c3281294212f06998f0f6b3c2
3yassu <3yassu@gmail.com>
2025-04-05 23:34:20 -0500

    added miri testing :)

---
+++ rusty_zelda/target/miri/x86_64-pc-windows-msvc/debug/deps/libc-5650f7bb83367d6d.d
@@ -0,0 +1,9 @@
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\miri\x86_64-pc-windows-msvc\debug\deps\liblibc-5650f7bb83367d6d.rmeta: C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\lib.rs C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\macros.rs C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\primitives.rs C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\windows\mod.rs C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\windows\msvc\mod.rs
+
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\miri\x86_64-pc-windows-msvc\debug\deps\libc-5650f7bb83367d6d.d: C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\lib.rs C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\macros.rs C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\primitives.rs C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\windows\mod.rs C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\windows\msvc\mod.rs
+
+C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\lib.rs:
+C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\macros.rs:
+C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\primitives.rs:
+C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\windows\mod.rs:
+C:\Users\eyass\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.171\src\windows\msvc\mod.rs:
---
+++ rusty_zelda/target/miri/x86_64-pc-windows-msvc/debug/deps/rusty_zelda-5243a73895208622.d
@@ -0,0 +1,12 @@
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\miri\x86_64-pc-windows-msvc\debug\deps\rusty_zelda-5243a73895208622.exe: src\main.rs src\game_runner.rs src\game_runner\objects\mod.rs src\game_runner\objects\item.rs src\game_runner\objects\felix.rs src\game_runner\objects\npc.rs src\game_runner\objects\world_runner.rs src\game_runner\objects\room_data.rs
+
+C:\Users\eyass\Programming Projects\RUST\Rusty-Zelda\rusty_zelda\target\miri\x86_64-pc-windows-msvc\debug\deps\rusty_zelda-5243a73895208622.d: src\main.rs src\game_runner.rs src\game_runner\objects\mod.rs src\game_runner\objects\item.rs src\game_runner\objects\felix.rs src\game_runner\objects\npc.rs src\game_runner\objects\world_runner.rs src\game_runner\objects\room_data.rs
+
+src\main.rs:
+src\game_runner.rs:
+src\game_runner\objects\mod.rs:
+src\game_runner\objects\item.rs:
+src\game_runner\objects\felix.rs:
+src\game_runner\objects\npc.rs:
+src\game_runner\objects\world_runner.rs:
+src\game_runner\objects\room_data.rs:
