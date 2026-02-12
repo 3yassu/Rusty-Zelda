586268b6afdeb309d47fd013c2e1892cb7a23341
3yassu <68251430+3yassu@users.noreply.github.com>
2026-02-12 17:39:54 -0600

    gitignore

--- rusty_zelda/target/debug/build/getrandom-dd5d1500bfa49ccd/build_script_build-dd5d1500bfa49ccd.d
+++
@@ -1,5 +0,0 @@
-/home/remon/personal_projects/hackku2025/Rusty-Zelda/rusty_zelda/target/debug/build/getrandom-dd5d1500bfa49ccd/build_script_build-dd5d1500bfa49ccd: /home/remon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.2/build.rs
-
-/home/remon/personal_projects/hackku2025/Rusty-Zelda/rusty_zelda/target/debug/build/getrandom-dd5d1500bfa49ccd/build_script_build-dd5d1500bfa49ccd.d: /home/remon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.2/build.rs
-
-/home/remon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.2/build.rs:
--- rusty_zelda/target/debug/deps/cfg_if-2b1095f2195c1f61.d
+++
@@ -1,7 +0,0 @@
-/home/remon/personal_projects/hackku2025/Rusty-Zelda/rusty_zelda/target/debug/deps/libcfg_if-2b1095f2195c1f61.rmeta: /home/remon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
-
-/home/remon/personal_projects/hackku2025/Rusty-Zelda/rusty_zelda/target/debug/deps/libcfg_if-2b1095f2195c1f61.rlib: /home/remon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
-
-/home/remon/personal_projects/hackku2025/Rusty-Zelda/rusty_zelda/target/debug/deps/cfg_if-2b1095f2195c1f61.d: /home/remon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
-
-/home/remon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs:
